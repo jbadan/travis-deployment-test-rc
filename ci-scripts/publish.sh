f0982a7f935018bf8748e06a506d20ac90ead48f
jenna-badanowski <j.badanowski@sap.com>
2019-01-02 10:13:31 -0800

    add my username/email

--- ci-scripts/publish.sh
+++ ci-scripts/publish.sh
@@ -1,5 +1,5 @@
-#git config --global user.email "EMAIL HERE"
-#git config --global user.name "USERNAME HERE"
+git config --global user.email "jenna.badanowski@gmail.com"
+git config --global user.name "jbadan"
 
 # update the package verion and commit to the git repository
 npm run std-version
