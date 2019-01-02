5e222ff517ca90cc20cef4326d016d4598ea46e8
jenna-badanowski <j.badanowski@sap.com>
2019-01-02 11:02:17 -0800

    test3

--- ci-scripts/publish_rc.sh
+++ ci-scripts/publish_rc.sh
@@ -2,6 +2,7 @@
 #git config --global user.name "USERNAME HERE"
 
 # update the package verion and commit to the git repository
+npm install
 npm run std-version -- --prerelease rc --no-verify
 git push --follow-tags origin ${CIRCLE_BRANCH}
 npm publish --tag prerelease
