7d6b2729cdb96d90138d15145751ae886dd087c6
jenna-badanowski <j.badanowski@sap.com>
2019-01-02 10:01:26 -0800

    add npm user

--- ci-scripts/publish_rc.sh
+++ ci-scripts/publish_rc.sh
@@ -1,1 +1,7 @@
-npm run std-version -- --prerelease rc
+#git config --global user.email "EMAIL HERE"
+#git config --global user.name "USERNAME HERE"
+
+# update the package verion and commit to the git repository
+npm run std-version -- --prerelease rc --no-verify
+git push --follow-tags origin ${CIRCLE_BRANCH}
+npm publish --tag prerelease
