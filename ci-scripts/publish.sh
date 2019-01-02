90f4d3a4ca956a66f33633037e8ed13826cb3dcb
jenna-badanowski <j.badanowski@sap.com>
2019-01-02 10:44:15 -0800

    test

--- ci-scripts/publish.sh
+++ ci-scripts/publish.sh
@@ -2,6 +2,9 @@ git config --global user.email "jenna.badanowski@gmail.com"
 git config --global user.name "jbadan"
 
 # update the package verion and commit to the git repository
+
+echo "made it here"
+
 npm run std-version
 
 git push --tags origin $TRAVIS_BRANCH
