cab8ee16f6e33028ce4229e0ebbc4830e499e8ab
jenna-badanowski <j.badanowski@sap.com>
2019-01-02 14:55:44 -0800

    testing

--- ci-scripts/publish.sh
+++ ci-scripts/publish.sh
@@ -1,5 +1,5 @@
-git config --global user.email "travis@travis-ci.org"
-git config --global user.name "Travis"
+git config --global user.email "jenna.badanowski@gmail.com"
+git config --global user.name "jbadan"
 
 # update the package verion and commit to the git repository
 
@@ -9,11 +9,13 @@ npm run std-version
 
 npm whoami
 
-npm adduser --registry=https://registry.npmjs.org/ <<!
-$NPM_USERNAME
-$NPM_PASSWORD
-$NPM_EMAIL
-!
+echo -e "$NPM_USERNAME\n$NPM_PASSWORD\n$NPM_EMAIL" | npm login
+
+# npm adduser --registry=https://registry.npmjs.org/ <<!
+# $NPM_USERNAME
+# $NPM_PASSWORD
+# $NPM_EMAIL
+# !
 
 git push --tags origin $TRAVIS_BRANCH
 
