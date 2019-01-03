b8d1df1905c6f9934b9afed0f08aeb5dc0f67eb9
jenna-badanowski <j.badanowski@sap.com>
2019-01-03 11:00:50 -0800

    push change

--- ci-scripts/publish.sh
+++ ci-scripts/publish.sh
@@ -1,5 +1,5 @@
-git config --global user.email "jenna.badanowski@gmail.com"
-git config --global user.name "jbadan"
+git config --global user.email "travis@travis.org"
+git config --global user.name "travis"
 
 # update the package verion and commit to the git repository
 
@@ -12,6 +12,10 @@ echo "//registry.npmjs.org/:_authToken=${AUTH_TOKEN}" >> ~/.npmrc
 echo "//registry.npmjs.org/:username=jbadan" >> ~/.npmrc
 echo "//registry.npmjs.org/:email=jenna.badanowski@gmail.com" >> ~/.npmrc
 
-git push "https://${GH_TOKEN}@github.com/jbadan/travis-deployment-test" --tags origin ${TRAVIS_BRANCH}
+echo ${TRAVIS_BRANCH}
+
+git push --force "https://${GITHUB_TOKEN}@$github.com/jbadan/travis-deployment-test.git" --tags  master > /dev/null 2>&1
+
+# git push "https://${GH_TOKEN}@github.com/jbadan/travis-deployment-test" --tags origin ${TRAVIS_BRANCH}
 
 npm publish
