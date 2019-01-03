73c18c67db999d7a5b3ac79268bdbafb4059dfb2
jenna-badanowski <j.badanowski@sap.com>
2019-01-03 12:41:23 -0800

    isolate travis branch

--- ci-scripts/publish.sh
+++ ci-scripts/publish.sh
@@ -1,66 +1,15 @@
-# function to make a commit on a branch in a Travis CI build
-# be sure to avoid creating a Travis CI fork bomb
-# see https://github.com/travis-ci/travis-ci/issues/1701
-# edited from https://gist.github.com/ddgenome/f3a60fe4c2af0cbe758556d982fbeea9
-function travis-branch-commit() {
-    local head_ref branch_ref
-    head_ref=$(git rev-parse HEAD)
-    if [[ $? -ne 0 || ! $head_ref ]]; then
-        err "failed to get HEAD reference"
-        return 1
-    fi
-    branch_ref=$(git rev-parse "$TRAVIS_BRANCH")
-    if [[ $? -ne 0 || ! $branch_ref ]]; then
-        err "failed to get $TRAVIS_BRANCH reference"
-        return 1
-    fi
-    if [[ $head_ref != $branch_ref ]]; then
-        msg "HEAD ref ($head_ref) does not match $TRAVIS_BRANCH ref ($branch_ref)"
-        msg "someone may have pushed new commits before this build cloned the repo"
-        return 0
-    fi
-    if ! git checkout "$TRAVIS_BRANCH"; then
-        err "failed to checkout $TRAVIS_BRANCH"
-        return 1
-    fi
-    if ! git add --all .; then
-        err "failed to add modified files to git index"
-        return 1
-    fi
-
-    local remote=origin
-    if [[ $GH_TOKEN ]]; then
-        remote=https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG
-    fi
-    if [[ $TRAVIS_BRANCH != master ]]; then
-        msg "not pushing updates to branch $TRAVIS_BRANCH"
-        return 0
-    fi
-    if ! git push --quiet --follow-tags "$remote" "$TRAVIS_BRANCH" > /dev/null 2>&1; then
-        err "failed to push git changes"
-        return 1
-    fi
-}
-function msg() {
-    echo "travis-commit: $*"
-}
-function err() {
-    msg "$*" 1>&2
-}
-
+#! /bin/bash
+. ci-scripts/travis_branch.sh
+# configure username and email for Travis
 git config --global user.email "travis@travis.org"
 git config --global user.name "travis"
 
 git checkout master
-
-# update the package verion and commit to the git repository
-
 npm install
-
+#bump version in package.json, changelog; git add and git commit
 npm run std-version
 
+# pushes changes to correct HEAD
 travis-branch-commit
-
-
 
 npm publish
