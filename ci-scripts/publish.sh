e4b866df47ad05a3aed0793c2c61b1862b1d82c5
jenna-badanowski <j.badanowski@sap.com>
2019-01-03 08:29:11 -0800

    test with github token

--- ci-scripts/publish.sh
+++ ci-scripts/publish.sh
@@ -12,6 +12,6 @@ echo "//registry.npmjs.org/:_authToken=${AUTH_TOKEN}" >> ~/.npmrc
 echo "//registry.npmjs.org/:username=jbadan" >> ~/.npmrc
 echo "//registry.npmjs.org/:email=jenna.badanowski@gmail.com" >> ~/.npmrc
 
-git push --tags origin $TRAVIS_BRANCH
+git push "https://${GH_API_KEY}@github.com/jbadan/travis-deployment-test.git" --tags origin $TRAVIS_BRANCH > /dev/null 2>&1
 
 npm publish
