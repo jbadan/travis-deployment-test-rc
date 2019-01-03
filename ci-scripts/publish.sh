96076f40da54282bf978e05030c998e7950478c9
jenna-badanowski <j.badanowski@sap.com>
2019-01-03 12:08:14 -0800

    wefwefeqe13e1

--- ci-scripts/publish.sh
+++ ci-scripts/publish.sh
@@ -1,7 +1,7 @@
 git config --global user.email "travis@travis.org"
 git config --global user.name "travis"
 
-git pull origin master
+git checkout master
 
 # update the package verion and commit to the git repository
 
@@ -13,10 +13,6 @@ echo "//registry.npmjs.org/:_password=${NPM_PASSWORD}" > ~/.npmrc
 echo "//registry.npmjs.org/:_authToken=${AUTH_TOKEN}" >> ~/.npmrc
 echo "//registry.npmjs.org/:username=jbadan" >> ~/.npmrc
 echo "//registry.npmjs.org/:email=jenna.badanowski@gmail.com" >> ~/.npmrc
-
-git status
-
-git commit -a -m "[CI SKIP]"
 
 git push "https://jbadan:${GH_TOKEN}@github.com/jbadan/travis-deployment-test.git" --tags origin ${TRAVIS_BRANCH}
 
