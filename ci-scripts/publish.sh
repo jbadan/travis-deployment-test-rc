d55ecbffa6d40b7be5c8f344c0ca31c4dc802c79
jenna-badanowski <j.badanowski@sap.com>
2019-01-03 11:07:51 -0800

    try 2

--- ci-scripts/publish.sh
+++ ci-scripts/publish.sh
@@ -12,9 +12,7 @@ echo "//registry.npmjs.org/:_authToken=${AUTH_TOKEN}" >> ~/.npmrc
 echo "//registry.npmjs.org/:username=jbadan" >> ~/.npmrc
 echo "//registry.npmjs.org/:email=jenna.badanowski@gmail.com" >> ~/.npmrc
 
-echo ${TRAVIS_BRANCH}
-
-git push --force "https://${GITHUB_TOKEN}@$github.com/jbadan/travis-deployment-test.git" --tags  master > /dev/null 2>&1
+git push --force "https://${GITHUB_TOKEN}@$github.com/jbadan/travis-deployment-test.git" --tags origin ${TRAVIS_BRANCH}
 
 # git push "https://${GH_TOKEN}@github.com/jbadan/travis-deployment-test" --tags origin ${TRAVIS_BRANCH}
 
