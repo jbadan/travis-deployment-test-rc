f568330e33094b71743fc85f87af520f06514341
jenna-badanowski <j.badanowski@sap.com>
2019-01-07 17:19:36 -0800

    testing

--- ci-scripts/publish.sh
+++ ci-scripts/publish.sh
@@ -3,20 +3,11 @@ git config --global user.email "travis@travis.org"
 git config --global user.name "travis"
 
 git checkout master
-npm install
-npm run build
+
 # update the package verion and commit to the git repository
-npm run std-version
+npm run std-version -- --prerelease rc --no-verify
 
 # pushes changes to master
 git push --quiet --follow-tags "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG" "$TRAVIS_BRANCH" > /dev/null 2>&1;
 
-# commit changes made by standard-version to develop branch
-git checkout develop
-git merge master
-git commit -a -m "chore: merge master into develop [ci skip]"
-git push --quiet --follow-tags "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG" develop > /dev/null 2>&1;
-
-# publish master to npm
-git checkout master
-npm publish
+npm publish --tag prerelease
