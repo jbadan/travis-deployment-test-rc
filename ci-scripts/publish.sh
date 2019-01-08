2598ae8735f2502bf8bea61a04477fbf8566d9a1
jenna-badanowski <j.badanowski@sap.com>
2019-01-08 09:10:20 -0800

    foo

--- ci-scripts/publish.sh
+++ ci-scripts/publish.sh
@@ -1,13 +1,17 @@
 #! /bin/bash
-git config --global user.email "travis@travis.org"
-git config --global user.name "travis"
+# npm publish for major versions
+if [ "$TRAVIS_TAG" ~= ^v\d+\.\d+\.\d+$ ]; then
+    npm publish
+else
+    git config --global user.email "travis@travis.org"
+    git config --global user.name "travis"
 
-git checkout master
+    git checkout master
+    # update the package verion and commit to the git repository
+    npm run std-version -- --prerelease rc --no-verify
 
-# update the package verion and commit to the git repository
-npm run std-version -- --prerelease rc --no-verify
+    # pushes changes to master
+    git push --quiet --follow-tags "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG" "$TRAVIS_BRANCH" > /dev/null 2>&1;
 
-# pushes changes to master
-git push --quiet --follow-tags "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG" "$TRAVIS_BRANCH" > /dev/null 2>&1;
-
-npm publish --tag prerelease
+    npm publish --tag prerelease
+fi
