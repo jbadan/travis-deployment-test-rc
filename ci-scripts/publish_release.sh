1dba07f5086e517c9c88425c3ddb79465b73f5d6
jenna-badanowski <j.badanowski@sap.com>
2019-01-14 13:52:50 -0800

    add echos

--- ci-scripts/publish_release.sh
+++ ci-scripts/publish_release.sh
@@ -25,26 +25,29 @@ hash_upstream=$(git rev-parse $git_branch@{upstream})
 
 #https://gist.github.com/devongovett/10399980
 pull_request() {
-  to_branch=$1
-  if [ -z $to_branch ]; then
-    to_branch="master"
-  fi
-  
+  to_branch="master"
   # try the upstream branch if possible, otherwise origin will do
   upstream=$(git config --get remote.upstream.url)
   origin=$(git config --get remote.origin.url)
   if [ -z $upstream ]; then
     upstream=$origin
   fi
+
+  echo "upstream $upstream"
   
   to_user=$(echo $upstream | sed -e 's/.*[\/:]\([^/]*\)\/[^/]*$/\1/')
+  echo "to_user $to_user"
   from_user=$(echo $origin | sed -e 's/.*[\/:]\([^/]*\)\/[^/]*$/\1/')
+  echo "from_user $from_user"
   repo=$(basename `git rev-parse --show-toplevel`)
+  echo "repo $repo"
   from_branch=$(git rev-parse --abbrev-ref HEAD)
+  echo "from_branch $from_branch"
   open "https://github.com/$to_user/$repo/pull/new/$to_user:$to_branch...$from_user:$from_branch"
 }
 
 set -o errexit
 
+git checkout -b chore/create-release
 git commit --allow-empty -m "chore(release): create new release via script"
 pull_request
