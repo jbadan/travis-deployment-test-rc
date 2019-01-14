ef42086ec42b61e4aeb65c0a899cfc682c330a95
jenna-badanowski <j.badanowski@sap.com>
2019-01-14 14:08:02 -0800

    try xdgopen

--- ci-scripts/publish_release.sh
+++ ci-scripts/publish_release.sh
@@ -23,31 +23,11 @@ hash_head=$(git rev-parse HEAD)
 hash_upstream=$(git rev-parse $git_branch@{upstream})
 [ "$hash_head" != "$hash_upstream" ] && echo -e "\n\t${ERROR}Sorry, the branch is out of date.${NOCOLOR}\n\tIn order to publish a release, the branch must match the origin.\n\n" && exit 0
 
-#https://gist.github.com/devongovett/10399980
-pull_request() {
-  to_branch="master"
-  # try the upstream branch if possible, otherwise origin will do
-  upstream=$(git config --get remote.upstream.url)
-  origin=$(git config --get remote.origin.url)
-  if [ -z $upstream ]; then
-    upstream=$origin
-  fi
-
-  echo "upstream $upstream"
-  
-  to_user=$(echo $upstream | sed -e 's/.*[\/:]\([^/]*\)\/[^/]*$/\1/')
-  echo "to_user $to_user"
-  from_user=$(echo $origin | sed -e 's/.*[\/:]\([^/]*\)\/[^/]*$/\1/')
-  echo "from_user $from_user"
-  repo=$(basename `git rev-parse --show-toplevel`)
-  echo "repo $repo"
-  from_branch=$(git rev-parse --abbrev-ref HEAD)
-  echo "from_branch $from_branch"
-  open "https://github.com/$to_user/$repo/pull/new/$to_user:$to_branch...$from_user:$from_branch"
-}
-
 set -o errexit
 
 git checkout -b chore/create-release
 git commit --allow-empty -m "chore(release): create new release via script"
-pull_request
+git push --set-upstream origin chore/create-release
+xdg-open https://github.com/jbadan/travis-deployment-test-rc/compare/master...chore/create-release
+git checkout master
+git branch -D chore/create-release
